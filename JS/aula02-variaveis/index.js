397f6d4d44737e09a02fbfb9109ef0176fb45172
ArianeSilva10 <francisca.silva@alu.ufc.br>
2024-10-03 15:39:02 -0300

    ADD JAVASCRIPT

---
+++ JS/aula02-variaveis/index.js
@@ -0,0 +1,2 @@
+const PI = 3.14545454;
+alert(PI);
---
+++ JS/aula04-StringToNumber/index.js
@@ -0,0 +1,8 @@
+let  numero1 = Number(prompt("Digite o primeiro número:"));
+let numero2 = Number(prompt("Digite o segundo número:"));
+
+let resultado = numero1 + numero2;
+
+document.write(resultado);
+
+console.log(resultado);
