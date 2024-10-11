9ed9bac1354bf5ec3eec5fc54bdc05bd683d4546
ArianeSilva10 <francisca.silva@alu.ufc.br>
2024-10-11 15:38:41 -0300

    ROTAS

---
+++ react/aula4-paginacao/src/routes/Paths.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import PageLayout from "../layouts/PageLayout";
+
+
+const Paths = () => {
+    return ( 
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<PageLayout />}>
+                        <Route index  element={<Home />}/>
+                        <Route path="/Products" element={<Products />}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+     );
+}
+ 
+export default Paths;
