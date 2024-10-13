918f2081bb158b789e04f4c5debc925a7fb65c71
ArianeSilva10 <francisca.silva@alu.ufc.br>
2024-10-13 16:51:29 -0300

    REQUISICOES

--- react/telaLogin/src/App.jsx
+++ react/telaLogin/src/App.jsx
@@ -3,10 +3,13 @@ import Paths from "./routes/Paths";
 import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
+import { AuthContext } from "./contexts/AuthContext";
 
 const App = () => {
   return ( 
-    <Paths />
+    <AuthContext>
+      <Paths />
+    </AuthContext>
    );
 }
  
--- react/telaLogin/src/routes/Paths.jsx
+++ react/telaLogin/src/routes/Paths.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
+import { Context } from "../contexts/AuthContext";
 
 const Paths = () => {
+
+    const {logado} = useContext(Context);
+
     return ( 
         <>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Login />} />
+                    {
+                        logado && (
+                            <>
+                                <Route path="/home" element={<Home/>}/>
+                            </>
+                        )
+                    }
                     <Route path="/dashboard" element={<Dashboard />} />
                 </Routes>
             </BrowserRouter>
