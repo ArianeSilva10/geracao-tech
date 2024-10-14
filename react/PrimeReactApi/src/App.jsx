160f4dbaf4621504842959d8a80a77cb30d9ce0f
ArianeSilva10 <francisca.silva@alu.ufc.br>
2024-10-14 17:43:39 -0300

    CONSUMINDO API

---
+++ react/PrimeReactApi/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Paths from "./routes/Paths";
+import "primeflex/primeflex.css"
+import "primeicons/primeicons.css"
+
+const App = () => {
+  return ( 
+    <>
+      <Paths />
+    </>
+   );
+}
+ 
+export default App;
