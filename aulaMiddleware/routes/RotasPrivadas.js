69ecd0025ac325bb70e0f33f5c4616670696579e
ArianeSilva10 <francisca.silva@alu.ufc.br>
2024-10-25 18:44:54 -0300

    MIDDLEWARE

---
+++ aulaMiddleware/routes/RotasPrivadas.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const UsuariosRotas = require('./UsuariosRotas');
+const PostsRotas = require('./PostRotas');
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const RotasPrivadas = express.Router();
+
+
+//Middleware
+RotasPrivadas.use((request, response, next) => {
+    let auth = false
+
+    if(request.headers.token){
+        const { token } = request.headers;
+
+        try{    
+            jwt.verify(token,  process.env.APP_KEY_TOKEN)
+            auth = true;
+        }catch(e){
+            return response.status(403).send(e);
+        }
+
+    }
+    
+    if(auth === false){
+        return response.status(403).send("Não Autorizado");        
+    }
+    next();
+})
+
+RotasPrivadas.use(UsuariosRotas);
+RotasPrivadas.use(PostsRotas);
+
+module.exports = RotasPrivadas;
---
+++ aulaMiddleware/routes/RotasPublicas.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const jwt = require('jsonwebtoken')
+const AuthController = require('../controllers/AuthController');
+require('dotenv').config()
+
+const RotasPublicas = express.Router();
+
+RotasPublicas.post('/login', (request, response) => {
+    const body = request.body;
+    const auth = new AuthController();
+    const dados = auth.login(body.login, body.senha);
+    if(dados){
+        const token = jwt.sign(dados, process.env.APP_KEY_TOKEN)
+        return response.json({
+            token: token
+        })
+    }
+})
+
+module.exports = RotasPrivadas;
